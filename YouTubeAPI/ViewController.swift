1d8a8c8f30ea47d10f09cc0b573d17d483c6f6a8
Vitaliy Butsan <skolkojvastut@gmail.com>
2022-05-10 14:26:08 +0300

    Changed files location

---
+++ YouTubeAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeAPI
+//
+//  Created by VitaliyButsan on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
