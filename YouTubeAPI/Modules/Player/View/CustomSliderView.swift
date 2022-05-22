f163f04c080b2372ae7b8daaa1ae5b0545ce4382
VitaliyButsan <skolkojvastut@gmail.com>
2022-05-22 22:48:04 +0300

    import SnapKit

--- YouTubeAPI/Modules/Player/View/CustomSliderView.swift
+++ YouTubeAPI/Modules/Player/View/CustomSliderView.swift
@@ -7,6 +7,7 @@
 
 import RxSwift
 import RxCocoa
+import SnapKit
 import UIKit
 
 class CustomSliderView: UIView {
--- YouTubeAPI/Modules/Player/View/PlayerView.swift
+++ YouTubeAPI/Modules/Player/View/PlayerView.swift
@@ -7,6 +7,7 @@
 
 import RxCocoa
 import RxSwift
+import SnapKit
 import UIKit
 import YouTubePlayer
 
