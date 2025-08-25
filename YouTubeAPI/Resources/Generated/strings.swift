7a78e2027b46af066f66d3b04d8d8b79d849622c
VadimSorokolit <macintosh@ukr.net>
2025-08-25 15:52:27 +0300

    Fixed Podfile, increased minimum iOS target, replaced channel IDs

--- YouTubeAPI/Resources/Generated/strings.swift
+++ YouTubeAPI/Resources/Generated/strings.swift
@@ -3,39 +3,42 @@
 
 import Foundation
 
-// swiftlint:disable superfluous_disable_command file_length implicit_return
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
 
 // MARK: - Strings
 
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
-  /// AIzaSyBxF4G3kTicXsQwmzspB1yVlMwfMSZDivk
-  internal static let apiKey = L10n.tr("Localizable", "apiKey")
+  /// Localizable.strings
+  ///   YouTubeAPI
+  /// 
+  ///   Created by VitaliyButsan on 10.05.2022.
+  internal static let apiKey = L10n.tr("Localizable", "apiKey", fallback: "AIzaSyBxF4G3kTicXsQwmzspB1yVlMwfMSZDivk")
   /// https://www.googleapis.com/youtube/v3
-  internal static let baseURL = L10n.tr("Localizable", "baseURL")
-  /// UC18bHNKD21woNT7G0F4PN7Q
-  internal static let channelId1 = L10n.tr("Localizable", "channelId1")
-  /// UCJhjE7wbdYAae1G25m0tHAA
-  internal static let channelId2 = L10n.tr("Localizable", "channelId2")
-  /// UCLr_PnLF0aClbzrEnpyxx0A
-  internal static let channelId3 = L10n.tr("Localizable", "channelId3")
-  /// UC9_DJQL7CPkR9PhjfRZAnlw
-  internal static let channelId4 = L10n.tr("Localizable", "channelId4")
+  internal static let baseURL = L10n.tr("Localizable", "baseURL", fallback: "https://www.googleapis.com/youtube/v3")
+  /// UCxNGwMtZo7ynegilkV8Dbkw
+  internal static let channelId1 = L10n.tr("Localizable", "channelId1", fallback: "UCxNGwMtZo7ynegilkV8Dbkw")
+  /// UCClZiNLIqCqMAiLDLETO6Zw
+  internal static let channelId2 = L10n.tr("Localizable", "channelId2", fallback: "UCClZiNLIqCqMAiLDLETO6Zw")
+  /// UCIm6Hxg4kwY79VTxQBsO2pA
+  internal static let channelId3 = L10n.tr("Localizable", "channelId3", fallback: "UCIm6Hxg4kwY79VTxQBsO2pA")
+  /// UCTRLjMPCWRwyTX6-MPlz-Gg
+  internal static let channelId4 = L10n.tr("Localizable", "channelId4", fallback: "UCTRLjMPCWRwyTX6-MPlz-Gg")
   /// brandingSettings, statistics
-  internal static let channelsRequestParts = L10n.tr("Localizable", "channelsRequestParts")
+  internal static let channelsRequestParts = L10n.tr("Localizable", "channelsRequestParts", fallback: "brandingSettings, statistics")
   /// PageControlCell
-  internal static let pageControlCellId = L10n.tr("Localizable", "pageControlCellId")
+  internal static let pageControlCellId = L10n.tr("Localizable", "pageControlCellId", fallback: "PageControlCell")
   /// PlaylistCell
-  internal static let playlistCellId = L10n.tr("Localizable", "playlistCellId")
+  internal static let playlistCellId = L10n.tr("Localizable", "playlistCellId", fallback: "PlaylistCell")
   /// PlaylistItemCell
-  internal static let playlistItemCellId = L10n.tr("Localizable", "playlistItemCellId")
+  internal static let playlistItemCellId = L10n.tr("Localizable", "playlistItemCellId", fallback: "PlaylistItemCell")
   /// snippet
-  internal static let playlistItemsRequestParts = L10n.tr("Localizable", "playlistItemsRequestParts")
+  internal static let playlistItemsRequestParts = L10n.tr("Localizable", "playlistItemsRequestParts", fallback: "snippet")
   /// snippet
-  internal static let playlistsRequestParts = L10n.tr("Localizable", "playlistsRequestParts")
+  internal static let playlistsRequestParts = L10n.tr("Localizable", "playlistsRequestParts", fallback: "snippet")
   /// statistics
-  internal static let videosRequestParts = L10n.tr("Localizable", "videosRequestParts")
+  internal static let videosRequestParts = L10n.tr("Localizable", "videosRequestParts", fallback: "statistics")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -43,8 +46,8 @@ internal enum L10n {
 // MARK: - Implementation Details
 
 extension L10n {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
